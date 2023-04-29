4050ee23c7eab790c1a8e0cb0e8cde90839f723a
SysSolutions <131492089+SysSolutionsPE@users.noreply.github.com>
2023-04-29 12:59:06 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,20 +37,21 @@ global.APIKeys = {
 global.Api = new (require('./plugins/neoxrApi.js'))(process.env.API_KEY)
 
 //settings
-global.owner = ['+447441437150',''] //owner number ++447441437150
-global.wame = ['+447441437150'] 
-global.pemilik = ['+447441437150'] //owner number ++447441437150
-global.premium = ['+447441437150'] //premium number ++447441437150
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.owner = ['+51945463812',''] //owner number ++447441437150
+global.wame = ['+51945463812'] 
+global.pemilik = ['+51945463812'] //owner number ++447441437150
+global.premium = ['+51945463812'] 
+ //premium number 
+global.pengguna = 'ⓈⓨⓢⓈⓞⓛⓤⓣⓘⓞⓝⓢ' //username
+global.footer = 'ⓈⓨⓢⓈⓞⓛⓤⓣⓘⓞⓝⓢ'
+global.botnma = 'ⓈⓨⓢⓈⓞⓛⓤⓣⓘⓞⓝⓢ' //bot name
+global.botname = 'ⓈⓨⓢⓈⓞⓛⓤⓣⓘⓞⓝⓢ'
+global.ownernma = 'ⓈⓨⓢⓈⓞⓛⓤⓣⓘⓞⓝⓢ' //owner name
+global.packname = 'ⓈⓨⓢⓈⓞⓛⓤⓣⓘⓞⓝⓢ' //sticker package name
+global.author = 'ⓈⓨⓢⓈⓞⓛⓤⓣⓘⓞⓝⓢ' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
-global.sp = '🔵' //sp
+global.sp = '🌐' //sp
 global.max_upload = 100
 global.dripstyping = false
 global.dripsreadgroup = false
